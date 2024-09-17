dba6931b8936f870f6a1decb9147fe04726428a8
Rez.pec <apecchintathum@gmail.com>
2024-09-17 22:49:54 +0700

    add: CRUD User

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"fmt"
+	"log"
+
 	"gorm.io/gorm"
 )
 
@@ -12,4 +15,42 @@ type User struct {
 	LastName       string
 	PictureProfile string
 	Bookmarks      []Post `gorm:"many2many:Bookmark;"`
+}
+
+func createUser(db *gorm.DB,user *User) {
+	result := db.Create(user)
+
+	if result.Error != nil{
+		log.Fatalf("Error creating user: %v",result.Error)
+	}
+	fmt.Println("Create User successfully")
+}
+
+func getUser(db *gorm.DB,id uint) *User{
+	var user User;
+	result := db.First(&user,id);
+
+	if result.Error != nil{
+		log.Fatalf("Error getting user: %v",result.Error);
+	}
+	return &user;
+}
+
+func updateUser(db *gorm.DB,user *User) {
+	result := db.Save(user)
+
+	if result.Error != nil{
+		log.Fatalf("Error updating user: %v",result.Error)
+	}
+	fmt.Println("Update User successfully")
+}
+
+func deleteUser(db *gorm.DB,id uint) {
+	var user User;
+	result := db.Delete(&user,id)
+
+	if result.Error != nil{
+		log.Fatalf("Error updating user: %v",result.Error)
+	}
+	fmt.Println("Update User successfully")
 }
