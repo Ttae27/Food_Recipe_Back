36934926766a75476034a3801c4af7f2b59740b4
Rez.pec <apecchintathum@gmail.com>
2024-10-23 17:20:52 +0700

    add: prevent same username

--- controller/user.go
+++ controller/user.go
@@ -10,14 +10,20 @@ import (
 	"gorm.io/gorm"
 )
 
-func CreateUser(db *gorm.DB,c *fiber.Ctx) error {
+func CreateUser(db *gorm.DB, c *fiber.Ctx) error {
 	input := new(models.User)
 
-	if err:= c.BodyParser(input); err != nil {
+	if err := c.BodyParser(input); err != nil {
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password),bcrypt.DefaultCost)
+	var existingUser models.User
+	if err := db.Where("username ILIKE ?", input.Username).First(&existingUser).Error; err == nil {
+		// If no error, that means a user with this username already exists
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
 	if err != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
@@ -26,74 +32,73 @@ func CreateUser(db *gorm.DB,c *fiber.Ctx) error {
 
 	result := db.Create(input)
 
-	if result.Error != nil{
+	if result.Error != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 	return c.JSON(input)
 }
 
-func GetUser(db *gorm.DB,c *fiber.Ctx) error{
-	var user models.User;
-	id,err := strconv.Atoi(c.Params("id"))
+func GetUser(db *gorm.DB, c *fiber.Ctx) error {
+	var user models.User
+	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
-	result := db.First(&user,id);
-	if result.Error != nil{
-		log.Fatalf("Error getting user: %v",result.Error);
+	result := db.First(&user, id)
+	if result.Error != nil {
+		log.Fatalf("Error getting user: %v", result.Error)
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
-	return c.JSON(user);
+	return c.JSON(user)
 }
 
-func UpdateUser(db *gorm.DB,c *fiber.Ctx) error {
+func UpdateUser(db *gorm.DB, c *fiber.Ctx) error {
 	user := new(models.User)
 	updatedUser := new(models.User)
 
-	id,err := strconv.Atoi(c.Params("id"))
+	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message":err})
-	}
-	
-	result := db.First(&user,id)
-	if result.Error != nil{
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message":result.Error})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": err})
 	}
 
-	if err := c.BodyParser(updatedUser);err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message":err})
+	result := db.First(&user, id)
+	if result.Error != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": result.Error})
+	}
+
+	if err := c.BodyParser(updatedUser); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": err})
 	}
 	if updatedUser.Password != "" {
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(updatedUser.Password),bcrypt.DefaultCost)
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(updatedUser.Password), bcrypt.DefaultCost)
 		if err != nil {
 			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		updatedUser.Password = string(hashedPassword)
-	} 
+	}
 
 	user.ID = uint(id)
 
 	updateResult := db.Model(&user).Updates(updatedUser)
-	if updateResult.Error != nil{
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message":result.Error})
+	if updateResult.Error != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": result.Error})
 	}
 	return c.JSON(updatedUser)
 }
 
-func DeleteUser(db *gorm.DB,c *fiber.Ctx) error {
-	var user models.User;
-	id,err := strconv.Atoi(c.Params("id"))
+func DeleteUser(db *gorm.DB, c *fiber.Ctx) error {
+	var user models.User
+	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
-	result := db.Delete(&user,id)
-	if result.Error != nil{
+	result := db.Delete(&user, id)
+	if result.Error != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 	return c.JSON("Deleted Successfully")
 }
-
